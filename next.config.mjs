77dcb4e6f21d2f7a0097b9195d8b19a099d2cd0e
harshLogicgo <harsh.logicgo@gmail.com>
2025-04-08 11:33:59 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPwa from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // other config options if any
+};
+
+// Wrap withPWA and export
+export default nextPwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
