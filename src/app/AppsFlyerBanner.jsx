e895143f63da5495fc20189fc622a63950ccfb85
harshLogicgo <harsh.logicgo@gmail.com>
2025-04-08 15:07:33 +0530

    changes

--- src/app/AppsFlyerBanner.jsx
+++ src/app/AppsFlyerBanner.jsx
@@ -1,30 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function AppsFlyerBanner() {
-  const [isMobile, setIsMobile] = useState(false);
-
   useEffect(() => {
-    const mobile = window.matchMedia("(max-width: 768px)").matches;
-    setIsMobile(mobile);
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+    if (!isMobile) return;
+
+    const script = document.createElement("script");
+    script.innerHTML = `
+    !function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AFfunction(){(t.AF.q=t.AF.q[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))},t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],o.async=1,o.src="https://websdk.appsflyer.com/?"+(c.length>0?"st="+c.split(",").sort().join(",")+"&":"")+(i.length>0?"af_id="+i:""),p.parentNode.insertBefore(o,p)}(window,document,"script",0,"AF","banners",{banners: {key: "b82895b6-0c56-47dd-bdd4-2b142eebec1c"}});
+    AF('banners', 'showBanner')
+    `;
+    document.body.appendChild(script);
+
+    return () => {
+      // Clean up if needed
+      document.body.removeChild(script);
+    };
   }, []);
 
-  if (!isMobile) return null;
-
-  return (
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-        !function(t,e,n,s,a,c,i,o,p){t.AppsFlyerSdkObject=a,t.AF=t.AF||function(){
-          (t.AF.q=t.AF.q||[]).push([Date.now()].concat(Array.prototype.slice.call(arguments)))
-        },t.AF.id=t.AF.id||i,t.AF.plugins={},o=e.createElement(n),p=e.getElementsByTagName(n)[0],
-        o.async=1,o.src="https://websdk.appsflyer.com/?st=banners&af_id=" + i,
-        p.parentNode.insertBefore(o,p)
-        }(window,document,"script",0,"AF","banners","YOUR_APPSFLYER_DEV_KEY");
-        AF('banners', 'showBanner');
-      `,
-      }}
-    />
-  );
+  return null;
 }
